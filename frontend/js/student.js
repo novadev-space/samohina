f8d9e70039368c4e75191d84c091611def983210
novadev-space <novaadev@proton.me>
2026-06-08 23:38:33 +0300

    Update student.js

--- frontend/js/student.js
+++ frontend/js/student.js
@@ -1,5 +1,5 @@
 // Адреса сервера для запитів
-const BASE_URL = window.API_URL || 'caring-respect-production-c61c.up.railway.app/api'; 
+const BASE_URL = window.API_URL || 'https://caring-respect-production-c61c.up.railway.app/api';
 
 // Розклад
 const LESSON_MAP_FOUR = {
